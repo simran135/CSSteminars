ec446024b6c2b955d690906773c4cfcd6bbd1639
ahanamuk <ahanamuk@andrew.cmu.edu>
2020-03-29 14:27:54 -0400

    functions, types, if else statements...need to add boolean operators

--- week1.py
+++ week1.py
@@ -10,6 +10,15 @@ def f():
 def add(x, y):
     return x + y
 
+# is this a valid function?
+def mystery(l[i], l[j]):
+    return l[i] + l[j]
+
+# is this a valid function?
+def smiley(l[i], l[j]):
+    return l[i] + l[j]
+
+
 ############################
 ##########  TYPES ##########
 ############################
@@ -35,5 +44,61 @@ print("And some more constants in the math module:")
 import math
 print(math.pi)
 print(math.e)
+
+############################
+########  IF ELSE ##########
+############################
+
+print("----------------")
+print("IF STATEMENTS\n")
+
+grade = 74
+if (grade < 60)
+    print("F")
+elif (grade < 70)
+    print("D")
+elif (grade < 80)
+    print("C")
+elif (grade < 90)
+    print("B")
+else:
+    print("YOU GOT AN A!!!")
+
+# One line if statement
+age = 15
+print('kid' if age < 18 else 'adult')
+
+# What do these print?
+l = ["1", "9", "10"]
+if l[1] < l[2]:
+   print (l[1], "<", l[2])
+else:
+   print (l[1], ">", l[2])
+
+if True:
+    print("is True true boys?...it seems so")
+
+if False:
+    print("Entered if statement")
+
+# Short circuiting
+print("illegal druggggzzz" if True else 1/0)
+
+
+#andalso, orelse, and, or
+
+############################
+####### OPERATORS ##########
+############################
+
+
+
+
+############################
+####### ERRORS #############
+############################
+
+
+
 
 
