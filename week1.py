1d623b66051950e92136608f4ff017c35faec63d
ahanamuk <ahanamuk@andrew.cmu.edu>
2020-03-29 13:01:31 -0400

    starting the week1 data types file

---
+++ week1.py
@@ -0,0 +1,39 @@
+
+############################
+#######  FUNCTIONS #########
+############################
+
+def f():
+    print("hello how are u today")
+    return 5
+
+def add(x, y):
+    return x + y
+
+############################
+##########  TYPES ##########
+############################
+
+print("Some basic types in Python:")
+print(type(2))           # int
+print(type(2.2))         # float
+
+print(type(2 < 2.2))     # bool (boolean)
+print(bool(0))           # False
+print(bool(5))           # True
+print(bool(1))           # True
+
+print(type("hello"))     # str
+print(type("2.2"))       # str (string or text)
+
+print("Some builtin constants:")
+print(True)
+print(False)
+print(None)
+
+print("And some more constants in the math module:")
+import math
+print(math.pi)
+print(math.e)
+
+
