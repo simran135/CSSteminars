43cb53b4b7b87a522193126984ca4c1c3d5c9f16
ahanamuk <ahanamuk@andrew.cmu.edu>
2020-03-30 12:52:46 -0400

    added notesheet to week 1, quiz, updated week 1 lesson

---
+++ quiz.py
@@ -0,0 +1,19 @@
+print("----------------")
+print("QUIZ TIME:\n")
+
+# Do the types match? Will these all compile?
+print(3 * 2)
+print(3 * "abc") 
+print(3 + 2)
+print("abc" + "def")
+print(3 + "def")
+
+print("Precedence:")
+print(2+3*4)  
+print(5+4%3)  
+print(2**3*4) 
+
+print()
+
+print("Associativity:")
+print(5-4-3) 
---
+++ week1_notesheet.py
@@ -0,0 +1,103 @@
+############################
+####  FUNCTIONS INTRO ######
+############################
+print("----------------")
+print("Functions in Python:")
+
+def f():
+    print("hello how are u today")
+    return 5
+
+def void_function():
+  print("this function doesn't return anything")
+
+def add(x, y):
+    return x + y
+
+add(1,3)
+
+############################
+###  TYPES & OPERATORS #####
+############################
+
+# Category	Operators
+# Arithmetic	+, -, , /, //, *, %, - (unary), + (unary)
+# Relational	<, <=, >=, >, ==, !=
+# Assignment	+=, -=, *=, /=, //=, **=, %=, <<=, >>=
+# Logical	and, or, not
+
+
+# Arithmetic	+, -, , /, //, *, %, - (unary), + (unary)
+print("----------------")
+print("Some basic types in Python:")
+print(type(2))           # int
+print(type(2.2))         # float
+print(type(2.0))         # float
+
+print("The / operator does 'normal' float division:")
+print(" 5/3  =", ( 5/3)) # 1.6666666666666667
+print()
+print("The // operator does integer division:")
+print(" 5//3 =", ( 5//3)) # 1
+print(" 2//3 =", ( 2//3)) # 0
+print(" -4//3 =", (-4//3)) # -1 (Rounds down towards negative infinity)
+
+
+# a = (a // b) * b + a % b 
+print(" 6%3 =", ( 6%3)) # 0
+print(" 5%3 =", ( 5%3)) # 2
+print(" 2%3 =", ( 2%3)) # 2
+print(" 0%3 =", ( 0%3)) # 0
+print(" 3%0 =", ( 3%0)) # ZeroDivisionError
+
+# Logical	and, or, not
+print(type(2 < 2.2))     # bool (boolean)
+print(bool(0))           # False
+print(bool(5))           # True
+print(bool(1))           # True
+
+print(type("hello"))     # str
+print(type("2.2"))       # str (string or text)
+
+print("Some builtin constants:")
+print(True)
+print(False)
+print(None)
+
+print("And some more constants in the math module:")
+import math
+print(math.pi)
+print(math.e)
+
+
+############################
+###  SHORT CIRCUITING ######
+############################
+
+# and statement
+def yes():
+    return True
+
+def no():
+    return False
+
+def crash():
+    return 1/0 # crashes!
+
+print(no() and crash()) # False
+print(crash() and no()) # crashes!
+print(yes() and crash()) # crashes!
+
+# or statement
+def yes():
+    return True
+
+def no():
+    return False
+
+def crash():
+    return 1/0 # crashes!
+
+print(yes() or crash()) # Works!
+print(crash() or yes()) # Crashes!
+print(no() or crash()) # crashes!
