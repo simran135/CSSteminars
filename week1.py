10b91bce582605852563056136d2ab69f92e7c75
ahanamuk <ahanamuk@andrew.cmu.edu>
2020-05-13 13:03:26 -0400

    summarized week1.py in header and updated the content in it

--- week1.py
+++ week1.py
@@ -1,3 +1,20 @@
+'''
+Summary:
+
+1) Introduce functions (similar to functions in math)
+    - Describe input/output
+    - Difference between a print and return statement
+    - Variable assignment: doesn't matter what the name 
+        of the variable is in the input
+2) Types and operators: 
+    - Arithmetic	+, -, *, /, //, **, %
+    - Take a short quiz
+3) Logical operators and short circuiting
+	- and, or, not
+    - Truth tables
+    - Follow up with examples in the section
+'''
+
 ############################
 ####  FUNCTIONS INTRO ######
 ############################
@@ -14,7 +31,7 @@ def void_function():
 def add(x, y):
     return x + y
 
-add(1,3)
+b = add(1,3)
 
 ############################
 ###  TYPES & OPERATORS #####
@@ -32,7 +49,7 @@ print("----------------")
 print("Some basic types in Python:")
 print(type(2))           # int
 print(type(2.2))         # float
-print(type(2.0))         # what type? 
+print(type(2.0))         # what type?
 
 print("The / operator does 'normal' float division:")
 print(" 5/3  =", ( 5/3))
@@ -45,9 +62,9 @@ print("-4//3 =", (-4//3))
 
 # a = (a // b) * b + a % b 
 print(" 6%3 =", ( 6%3))
-print(" 5%3 =", ( 5%3))
+print(" 5%3 =", ( 5%3)) 
 print(" 2%3 =", ( 2%3))
-print(" 0%3 =", ( 0%3))
+print(" 0%3 =", ( 0%3)) 
 print("-4%3 =", (-4%3))
 print("-4%3 =", (-16%3))
 print(" 3%0 =", ( 3%0))
@@ -75,106 +92,33 @@ import math
 print(math.pi)
 print(math.e)
 
+############################
+########## QUIZ ############
+############################
 
 print("----------------")
 print("QUIZ TIME:\n")
 
 # Do the types match? Will these all compile?
 print(3 * 2)
-print(3 * "abc")
+print(3 * "abc") 
 print(3 + 2)
-print("abc" + "def")
+print("abc" + "def") #string concatenation
 print(3 + "def")
 
 print("Precedence:")
-print(2+3*4)  # prints 14, not 20
-print(5+4%3)  # prints  6, not 0 (% has same precedence as *, /, and //)
-print(2**3*4) # prints 32, not 4096 (** has higher precedence than *, /, //, and %)
+print(2+3*4)  #14 = 2 + 12
+print(5+4%3)  #6 = 5 + 1
+print(2**3*4) #
 
 print()
 
 print("Associativity:")
-print(5-4-3)   # prints -2, not 4 (- associates left-to-right)
-
-print(0.1 + 0.1 == 0.2)        # True, but...
-print(0.1 + 0.1 + 0.1 == 0.3)  # False!
-print(0.1 + 0.1 + 0.1)         # prints 0.30000000000000004 (uh oh)
-print((0.1 + 0.1 + 0.1) - 0.3) # prints 5.55111512313e-17 (tiny, but non-zero!)
-
-print("The problem....")
-d1 = 0.1 + 0.1 + 0.1
-d2 = 0.3
-print(d1 == d2)                # False (never use == with floats!)
-
-print()
-print("The solution...")
-epsilon = 10**-10
-print(abs(d2 - d1) < epsilon)  # True!
-
-print()
-print("Once again, using a useful helper function, almostEqual:")
-
-def almostEqual(d1, d2):
-    epsilon = 10**-10
-    return (abs(d2 - d1) < epsilon)
-
-d1 = 0.1 + 0.1 + 0.1
-d2 = 0.3
-print(d1 == d2)            # still False, of course
-print(almostEqual(d1, d2)) # True, and now packaged in a handy reusable function!
+print(5-4-3)   
 
 ############################
-########  IF ELSE ##########
+# BOOLEAN SHORT-CIRCUITING #
 ############################
-
-print("----------------")
-print("IF STATEMENTS\n")
-
-grade = 74
-if (grade < 60):
-    print("F")
-elif (grade < 70):
-    print("D")
-elif (grade < 80):
-    print("C")
-elif (grade < 90):
-    print("B")
-else:
-    print("YOU GOT AN A!!!")
-
-if (grade < 60):
-    print("F")
-if (grade < 70):
-    print("D")
-if (grade < 80):
-    print("C")
-if (grade < 90):
-    print("B")
-if (grade < 100):
-    print("YOU GOT AN A!!!")
-
-# ONE LINE IF STATEMENT
-
-age = 15
-print('kid' if age < 18 else 'adult')
-
-# What do these print?
-l = ["1", "9", "10"]
-if l[1] < l[2]:
-   print (l[1], "<", l[2])
-else:
-   print (l[1], ">", l[2])
-
-if True:
-    print("is True true boys?...it seems so")
-
-if False:
-    print("Entered if statement")
-
-# SHORT CIRCUITING
-
-# What would these print?
-print("illegal druggggzzz" if True else 1/0)
 
 # and statement
 def yes():
