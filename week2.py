b473e17eb52bb2cabc7d6d6616a59ba9601278ed
ahanamuk <ahanamuk@andrew.cmu.edu>
2020-05-13 13:47:24 -0400

    updating week2.py, need to finish the functions section

--- week2.py
+++ week2.py
@@ -52,7 +52,22 @@ print(almostEqual(d1, d2)) # True, and now packaged in a handy reusable function
 print("----------------")
 print("IF STATEMENTS\n")
 
+# if (this boolean expression evaluates to True):
+#   this block of code gets executed
+
+if True:
+    print("true")
+
+if False:
+    print("false")
+
+if (3 == 3):
+    print("true")
+else:
+    print("false")
+
 grade = 74
+
 if (grade < 60):
     print("F")
 elif (grade < 70):
@@ -88,31 +103,63 @@ else:
     print("9 < 10")
 
 if True:
-    print("is True true boys?...it seems so")
+    print("is True true guys?...it seems so")
 
 if False:
     print("Entered if statement")
 
 ############################
-#######  VARIABLES #########
-############################
-
-
-############################
-# STATEMENTS AND EXPRESSIONS 
-############################
-
-
-
-
-############################
 #######  FUNCTIONS #########
 ############################
 
+'''
+Vocab:
+    - function name
+    - parameters
+    - arguments
+    - function call
+    - return value
+    - return type
+    - statement
+    - expression
+    - global variables
+    - local variables
+'''
+
+y = 7
+
+# Can you make the return type of this function a float? An int?
+def functionName(x, z):
+    result = x + z + y
+    return result
+
+print(functionName(1, 3))
+print(functionName(2, 4))
+print(functionName(True, False))
+
+# What do these functions return?
+
+def isEven(x):
+    print("hey")
+    return (x % 2 == 0)
+    print("wassup")
+
+print(isEven(5))
+print(isEven(4))
+
+#------------------------
+
+def printing(y):
+    print(y)
+
+a = printing(1)
+print(a)
+
+
 
 # is this a valid function?
-# def mystery(l[i], l[j]):
-#     return l[i] + l[j]
+def mystery(i + j, x):
+    return i + j + x
 
 # is this a valid function?
 def mystery(smiley, i):
